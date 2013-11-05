110a98f69f354ece901077beff7111fa8da41673
Josh Tan <jtan189@gmail.com>
2013-11-05 00:31:34 -0600

    Refactored code.

--- TanAssig2.R
+++ TanAssig2.R
@@ -1,93 +1,77 @@
 # Implementation of the Naive Bayes Classifier in R
 ## Author: Josh Tan
 
-# You can use setwd("C:/YourPATH here/") to set the working directory..
+# TODO: employ Laplace correction
 
 # input parameters
-training.file = "AssigData2/irisPCTraining.txt"
-testing.file = "AssigData2/irisPCTesting.txt"
+train.file = "ExampleDatasets/irisTraining.txt"
+test.file = "ExampleDatasets/irisTesting.txt"
 
-train = as.matrix(read.table(training.file))
-num.rows = dim(train)[1]
-num.cols = dim(train)[2] - 1 # the last attribute is the class label, so it does not count.
+# TRAINING
 
-# <test>
-#cat("training data:\n", trainingMatrix)
-#cat("rows: ", n, "\n")
-#cat("cols: ", d, "\n")
-# </test>
+# read in training data
+train.data = as.matrix(read.table(train.file))
+train.nrows = nrow(train.data)
+ncols = ncol(train.data) - 1 # last attribute is the class label
 
-#Training... Collect mean and standard deviation for each dimension for each class..
-#Also, calculate P(C+) and P(C-)
+## separate data based on class
+train.data.pos = which(train.data[, (ncols + 1)] == 1)
+train.data.neg = which(train.data[, (ncols + 1)] == -1)
 
-train.pos.rows = which(train[, (num.cols + 1)] == 1)
-train.neg.rows = which(train[, (num.cols + 1)] == -1)
+## for each class, calculate means for each attribute
+train.data.pos.mean = colMeans(train.data[train.data.pos, 1:ncols])
+train.data.neg.mean = colMeans(train.data[train.data.neg, 1:ncols])
 
-train.pos.colmeans = colMeans(train[train.pos.rows, 1:num.cols])
-train.neg.colmeans = colMeans(train[train.neg.rows, 1:num.cols])
+## for each class, calculate standard deviations for each attribute
+train.data.pos.sd = apply(train.data[train.data.pos, 1:ncols], 2, sd)
+train.data.neg.sd = apply(train.data[train.data.neg, 1:ncols], 2, sd)
 
-# http://nsaunders.wordpress.com/2010/08/20/a-brief-introduction-to-apply-in-r/
+## for each class, calculate priors
+prior.pos = length(train.data.pos) / train.nrows
+prior.neg = length(train.data.neg) / train.nrows
 
-train.pos.sd = apply(train[train.pos.rows, 1:num.cols], 2, sd)
-train.neg.sd = apply(train[train.neg.rows, 1:num.cols], 2, sd)
+# TESTING
 
-prob.pos = length(train.pos.rows) / num.rows
-prob.neg = length(train.neg.rows) / num.rows
+# read in testing data
+test.data = as.matrix(read.table(test.file))
+test.nrows = nrow(test.data)
 
-#Testing .....
-
-test = as.matrix(read.table(testing.file))
-test.numrows = dim(test)[1] # Number of points in the testing data.
-
-prob.test.pos <- rep(1, test.numrows)
-prob.test.neg <- rep(1, test.numrows)
+# initialize likelihood measures and test class predictions
+post.pos <- rep(prior.pos, test.nrows)
+post.neg <- rep(prior.neg, test.nrows)
 test.pred = vector()
 
-# assessment
+# initialize assessment variables
 true.pos = 0;
 true.neg = 0;
 false.pos = 0;
 false.neg = 0;
 
-for (i in 1:test.numrows) {
+for (i in 1:test.nrows) {
 
-    for (j in 1:num.cols) {
-
-        prob.test.pos[i] = prob.test.pos[i] * dnorm(test[i,j], train.pos.colmeans[j], train.pos.sd[j])
-        prob.test.neg[i] = prob.test.neg[i] * dnorm(test[i,j], train.neg.colmeans[j], train.neg.sd[j])
-
-        ## cat("prob: ", prob.test.pos[i], "\n")
-        ## if (is.na(prob.test.pos[i])) {
-        ##     print("wtf!!!!!!! POS \n")
-        ##     cat("i: ", i, "; j: ", j, "\n")
-        ## }
-
-        ## if (is.na(prob.test.neg[i])) {
-        ##     print("wtf!!!!!!! NEG \n")
-        ##     cat("i: ", i, "; j: ", j, "\n")
-        ## }
+    for (j in 1:ncols) {
+        
+        # calculate numerator of posteriori (likelihood * prior)
+        post.pos[i] = post.pos[i] * dnorm(test.data[i,j], train.data.pos.mean[j], train.data.pos.sd[j])
+        post.neg[i] = post.neg[i] * dnorm(test.data[i,j], train.data.neg.mean[j], train.data.neg.sd[j])
     }
 
-    prob.test.pos[i] = prob.test.pos[i] * prob.pos
-    prob.test.neg[i] = prob.test.neg[i] * prob.neg
-
-    if (prob.test.pos[i] > prob.test.neg[i]) {
+    # compare posteriors to determine which is greater; corresponding class will be used
+    if (post.pos[i] >= post.neg[i]) { # if equal, just choose the positive class
         test.pred[i] = 1
-    } else if (prob.test.pos[i] < prob.test.neg[i]) {
-        test.pred[i] = -1
     } else {
-        test.pred[i] = 0 # add logic to pick one
-    }
+        test.pred[i] = -1
+    } 
 
-    ## cat("pred: ", test.pred[i], "\n")
+    # increment appropriate assessment variable
     if (test.pred[i] == 1) {
-        if (test[i, num.cols + 1] == 1) {
+        if (test.data[i, ncols + 1] == 1) {
             true.pos = true.pos + 1
         } else {
             false.pos = false.pos + 1
         }
     } else {
-        if (test[i, num.cols + 1] == -1) {
+        if (test.data[i, ncols + 1] == -1) {
             true.neg = true.neg + 1
         } else {
             false.neg = false.neg + 1
@@ -95,30 +79,15 @@ for (i in 1:test.numrows) {
     }
 }
 
-accuracy = (true.pos + true.neg) / test.numrows
+# calculate performance metrics
+accuracy = (true.pos + true.neg) / test.nrows
 precision = true.pos / (true.pos + false.pos)
 recall = true.pos / (true.pos + false.neg)
 
-cat("accuracy: ", accuracy, "\n")
-
-cat("true pos: ", true.pos, "\n")
-cat("false pos: ", false.pos, "\n")
-cat("true neg: ", true.neg, "\n")
-cat("false neg: ", false.neg, "\n")
-
-cat("precision: ", precision, "\n")
-cat("recall: ", recall, "\n")
-
-
-#for (i in 1:nn) {
-
-  #For each point find the P(C+|Xi) and P(C-|Xi) and decide if the point belongs to C+ or C-..
-  #Recall we need to calculate P(Xi|C+)*P(C+) ..
-  #P(Xi|C+) = P(Xi1|C+) * P(Xi2|C+)....P(Xid|C+)....Do the same for P(Xi|C-)
-  #Now that you've calculate P(Xi|C+) and P(Xi|C-), we can decide which is higher 
-  #P(Xi|C-)*P(C-) or P(Xi|C-)*P(C-) ..
-  #increment TP,FP,FN,TN accordingly, remember the true label for the ith point is in Xtest[i,(d+1)]
-
-#}
-
-# Calculate all the measures required..
+cat("True Positives: ", true.pos, "\n")
+cat("False Positives: ", false.pos, "\n")
+cat("True Negatives: ", true.neg, "\n")
+cat("False Negatives: ", false.neg, "\n")
+cat("Accuracy: ", accuracy, "\n")
+cat("Precision: ", precision, "\n")
+cat("Recall: ", recall, "\n")
