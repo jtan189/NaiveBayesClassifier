83a14adca7008a8f6ee2b0c7b6defc43a3ad74f9
Josh Tan <jtan189@gmail.com>
2013-11-05 21:00:18 -0600

    Removed unnecessary for loop. Changed some variable names.

--- TanAssig2.R
+++ TanAssig2.R
@@ -1,47 +1,45 @@
-# Implementation of the Naive Bayes Classifier in R
+## Implementation of the Naive Bayes Classifier in R
 ## Josh Tan
 ## CSCI 479
 ## 11/5/13
 
-# input parameters
-train.file = "ExampleDatasets/irisTraining.txt"
-test.file = "ExampleDatasets/irisTesting.txt"
+## input parameters
+train.file = "ExampleDatasets/irisPCTraining.txt"
+test.file = "ExampleDatasets/irisPCTesting.txt"
 
-# TRAINING
+## TRAINING
 
-# read in training data
+## read in training data
 train.data = as.matrix(read.table(train.file))
 train.nrows = nrow(train.data)
 ncols = ncol(train.data) - 1 # last attribute is the class label
 
 ## separate data based on class
-train.data.pos = which(train.data[, (ncols + 1)] == 1)
-train.data.neg = which(train.data[, (ncols + 1)] == -1)
+train.pos = which(train.data[, (ncols + 1)] == 1)
+train.neg = which(train.data[, (ncols + 1)] == -1)
 
 ## for each class, calculate means for each attribute
-train.data.pos.mean = colMeans(train.data[train.data.pos, 1:ncols])
-train.data.neg.mean = colMeans(train.data[train.data.neg, 1:ncols])
+train.pos.mean = colMeans(train.data[train.pos, 1:ncols])
+train.neg.mean = colMeans(train.data[train.neg, 1:ncols])
 
 ## for each class, calculate standard deviations for each attribute
-train.data.pos.sd = apply(train.data[train.data.pos, 1:ncols], 2, sd)
-train.data.neg.sd = apply(train.data[train.data.neg, 1:ncols], 2, sd)
+train.pos.sd = apply(train.data[train.pos, 1:ncols], 2, sd)
+train.neg.sd = apply(train.data[train.neg, 1:ncols], 2, sd)
 
 ## for each class, calculate priors
-prior.pos = length(train.data.pos) / train.nrows
-prior.neg = length(train.data.neg) / train.nrows
+prior.pos = length(train.pos) / train.nrows
+prior.neg = length(train.neg) / train.nrows
 
-# TESTING
+## TESTING
 
-# read in testing data
+## read in testing data
 test.data = as.matrix(read.table(test.file))
 test.nrows = nrow(test.data)
 
-# initialize posteriors and class predictions
-post.pos <- rep(prior.pos, test.nrows)
-post.neg <- rep(prior.neg, test.nrows)
+## initialize class predictions for test data
 test.pred = vector()
 
-# initialize assessment variables
+## initialize assessment variables
 true.pos = 0;
 true.neg = 0;
 false.pos = 0;
@@ -49,21 +47,22 @@ false.neg = 0;
 
 for (i in 1:test.nrows) {
 
-    for (j in 1:ncols) {
-        
-        # calculate numerator of posteriori (likelihood * prior)
-        post.pos[i] = post.pos[i] * dnorm(test.data[i,j], train.data.pos.mean[j], train.data.pos.sd[j])
-        post.neg[i] = post.neg[i] * dnorm(test.data[i,j], train.data.neg.mean[j], train.data.neg.sd[j])
-    }
+    ## for each class, calculate likelihoods
+    likelihood.pos = dnorm(test.data[i,1:ncols], train.pos.mean, train.pos.sd)
+    likelihood.neg = dnorm(test.data[i,1:ncols], train.neg.mean, train.neg.sd)
 
-    # compare posteriors to determine which is greater; corresponding class will be used
+    ## posterior = likelikehood * prior
+    post.pos[i] = prod(likelihood.pos) * prior.pos
+    post.neg[i] = prod(likelihood.neg) * prior.neg
+    
+    ## compare posteriors to determine which is greater; corresponding class will be used
     if (post.pos[i] >= post.neg[i]) { # if equal, just choose the positive class
         test.pred[i] = 1
     } else {
         test.pred[i] = -1
     } 
 
-    # increment appropriate assessment variable
+    ## increment appropriate assessment variables
     if (test.pred[i] == 1) {
         if (test.data[i, ncols + 1] == 1) {
             true.pos = true.pos + 1
