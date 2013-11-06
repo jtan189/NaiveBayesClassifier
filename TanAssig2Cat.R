50bb758e24b9518b2dd74546bcc8f69454cc9c35
Josh Tan <jtan189@gmail.com>
2013-11-05 18:45:26 -0600

    Added emacs temp files to .gitignore. Updated TanAssig2Cat.R to employ Laplace corrections.

--- TanAssig2Cat.R
+++ TanAssig2Cat.R
@@ -1,15 +1,15 @@
-# Implementation of the Naive Bayes Classifier in R (for categorical attributes)
+## Implementation of the Naive Bayes Classifier in R (for categorical attributes)
 ## Josh Tan
 ## CSCI 479
 ## 11/5/13
 
-# input parameters
+## input parameters
 train.file = "ExampleDatasets/buyTraining.txt"
 test.file = "ExampleDatasets/buyTesting.txt"
 
-# TRAINING
+## TRAINING
 
-# read in training data
+## read in training data
 train.data = as.matrix(read.table(train.file))
 train.nrows = nrow(train.data)
 ncols = ncol(train.data) - 1 # last attribute is the class label
@@ -18,34 +18,22 @@ ncols = ncol(train.data) - 1 # last attribute is the class label
 train.data.pos = which(train.data[, (ncols + 1)] == 1)
 train.data.neg = which(train.data[, (ncols + 1)] == -1)
 
-## ## for each class, calculate means for each attribute
-## train.data.pos.mean = colMeans(train.data[train.data.pos, 1:ncols])
-## train.data.neg.mean = colMeans(train.data[train.data.neg, 1:ncols])
-
-## ## for each class, calculate standard deviations for each attribute
-## train.data.pos.sd = apply(train.data[train.data.pos, 1:ncols], 2, sd)
-## train.data.neg.sd = apply(train.data[train.data.neg, 1:ncols], 2, sd)
-
 ## for each class, calculate priors
 prior.pos = length(train.data.pos) / train.nrows
 prior.neg = length(train.data.neg) / train.nrows
 
+## TESTING
 
-## as.data.frame(table(train.data[,1]))
-
-
-# TESTING
-
-# read in testing data
+## read in testing data
 test.data = as.matrix(read.table(test.file))
 test.nrows = nrow(test.data)
 
-# initialize posteriors and class predictions
+## initialize posteriors and class predictions
 post.pos <- rep(prior.pos, test.nrows)
 post.neg <- rep(prior.neg, test.nrows)
 test.pred = vector()
 
-# initialize assessment variables
+## initialize assessment variables
 true.pos = 0;
 true.neg = 0;
 false.pos = 0;
@@ -55,14 +43,23 @@ for (i in 1:test.nrows) {
 
     for (j in 1:ncols) {
         ## calculate numerator of posteriori (likelihood * prior)
-        num.occur = length(which(train.data[,j] == test.data[i,j]))
+        num.occur = length(which(train.data[,j] == test.data[i,j])) + 1 # apply laplace correction by adding 1
 
-        if (num.occur == 0) {
-            print("Do something.\n")
-        }
+        denom = train.nrows + length(unique(test.data[,j])) + length(unique(train.data[,j])) # add in number of unique categories
 
-        post.pos[i] = post.pos[i] * (num.occur / train.nrows)
-        post.neg[i] = post.neg[i] * (num.occur / train.nrows)
+        post.pos[i] = post.pos[i] * (num.occur / denom)
+        post.neg[i] = post.neg[i] * (num.occur / denom)
+
+        ## <debug>
+        print("-------\n")
+        cat('post.pos: ', post.pos[i], "\n")
+        cat('post.neg: ', post.neg[i], "\n")
+        cat('prior.pos: ', prior.pos, "\n")
+        cat('prior.neg: ', prior.neg, "\n")
+        cat('numer: ', num.occur, "\n")
+        cat('denom: ', denom, "\n")
+        print("-------\n")
+        ## </debug>
     }
 
     # compare posteriors to determine which is greater; corresponding class will be used
